b5028b9c44abe66aa24a29356729272d019320c1
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-05-18 15:21:42 +0600

    ui design register

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen bg-secondary'>
+        <div className='w-full max-w-md space-y-6 p-8 bg-white shadow-lg rounded-lg'>
+                <div className='text-2xl font-bold text-center text-primary'>
+                    <h1>Create your account</h1>
+
+                </div>
+              
+        </div>
+      
+
+    </div>
+  )
+}
+
+export default page
